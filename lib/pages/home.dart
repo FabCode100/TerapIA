fa89f2949871282db4be89d90058b119c5ce8bcd
Fabriciob45 <kacchanhero488@gmail.com>
2023-08-18 18:49:25 -0300

    Basis

---
+++ lib/pages/home.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

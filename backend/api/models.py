e904f4759c3d843dc1ca287750d502d1302c2308
Fabriciob45 <kacchanhero488@gmail.com>
2023-09-01 23:51:25 -0300

    ModelsConfigAndResgistration

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# User Table
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+# Conversation Table
+class Conversation(models.Model):
+    user = models.ForeignKey(UserProfile, on_delete=models.CASCADE) 
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+# Message Table
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    sender = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    emotion = models.CharField(max_length=255)
+    advice = models.TextField()
+

5b209e6d9ee826c46343baf727a925f6504fb64e
Fabriciob45 <kacchanhero488@gmail.com>
2023-09-08 21:00:47 -0300

    ChatAnimationsCompleted

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,7 +3,9 @@ import 'package:frontend/pages/chat.dart';
 import 'package:frontend/pages/signup.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
